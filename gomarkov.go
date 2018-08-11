02758deb658876621561151bca41d1ecebac4040
Miroojin Bakshi <mbakshi1094@gmail.com>
2018-08-11 13:20:25 +0530

    Move utilities to helpers.go

--- gomarkov.go
+++ gomarkov.go
@@ -2,22 +2,8 @@ package gomarkov
 
 import (
 	"errors"
-	"strings"
 	"sync"
 )
-
-//NGram ...
-type NGram []string
-
-func (ngram NGram) key() string {
-	return strings.Join(ngram, "_")
-}
-
-//Pair is a pair of consecutive states in a sequece
-type Pair struct {
-	CurrentState NGram  // n = order of the chain
-	NextState    string // n = 1
-}
 
 //Chain is a markov chain instance
 type Chain struct {
@@ -25,16 +11,6 @@ type Chain struct {
 	statePool    *spool
 	frequencyMat map[int]sparseArray
 	sync.RWMutex
-}
-
-type sparseArray map[int]int
-
-func (s sparseArray) sum() int {
-	sum := 0
-	for _, count := range s {
-		sum += count
-	}
-	return sum
 }
 
 //NewChain creates an instance of Chain
@@ -67,14 +43,6 @@ func (chain *Chain) Add(input []string) {
 	}
 }
 
-func fill(value string, count int) []string {
-	arr := make([]string, count)
-	for i := range arr {
-		arr[i] = value
-	}
-	return arr
-}
-
 //TransitionProbability returns the transition probability between two states
 func (chain *Chain) TransitionProbability(next string, current NGram) (float64, error) {
 	if len(current) != chain.Order {
@@ -89,24 +57,4 @@ func (chain *Chain) TransitionProbability(next string, current NGram) (float64, 
 	sum := float64(arr.sum())
 	freq := float64(arr[nextIndex])
 	return freq / sum, nil
-}
-
-//MakePairs generates n-gram pairs of consecutive states in a sequence
-func MakePairs(tokens []string, order int) []Pair {
-	var pairs []Pair
-	for i := 0; i < len(tokens)-order; i++ {
-		pair := Pair{
-			CurrentState: tokens[i : i+order],
-			NextState:    tokens[i+order],
-		}
-		pairs = append(pairs, pair)
-	}
-	return pairs
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
 }
---
+++ helpers.go
@@ -0,0 +1,54 @@
+package gomarkov
+
+import "strings"
+
+//Pair is a pair of consecutive states in a sequece
+type Pair struct {
+	CurrentState NGram  // n = order of the chain
+	NextState    string // n = 1
+}
+
+//NGram is a array of tokens
+type NGram []string
+
+type sparseArray map[int]int
+
+func (ngram NGram) key() string {
+	return strings.Join(ngram, "_")
+}
+
+func (s sparseArray) sum() int {
+	sum := 0
+	for _, count := range s {
+		sum += count
+	}
+	return sum
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func fill(value string, count int) []string {
+	arr := make([]string, count)
+	for i := range arr {
+		arr[i] = value
+	}
+	return arr
+}
+
+//MakePairs generates n-gram pairs of consecutive states in a sequence
+func MakePairs(tokens []string, order int) []Pair {
+	var pairs []Pair
+	for i := 0; i < len(tokens)-order; i++ {
+		pair := Pair{
+			CurrentState: tokens[i : i+order],
+			NextState:    tokens[i+order],
+		}
+		pairs = append(pairs, pair)
+	}
+	return pairs
+}
